c87c78c68c0302e87c4b0c75ccc4664fa8ef7964
Amit <vamit20000@gmail.com>
2026-01-13 21:03:42 +0530

    twilio done

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,15 +1,30 @@
-import express from 'express';
-import { signup, login, logout, getMe, getAllBusinessUsers, uploadAvatar, updateUserAvatar, removeUserAvatar } from '../controller/authController.js';
-import authenticate from '../middleware/authenticate.js';
+import express from "express";
+import {
+  signup,
+  login,
+  logout,
+  getMe,
+  getAllBusinessUsers,
+  uploadAvatar,
+  updateUserAvatar,
+  removeUserAvatar,
+  sendOTP,
+  verifyOTP,
+} from "../controller/authController.js";
+import authenticate from "../middleware/authenticate.js";
 
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/login', login);
-router.post('/logout', logout);
-router.get('/business-users', getAllBusinessUsers); 
-router.get('/me', authenticate, getMe);
-router.post('/upload-avatar', authenticate, uploadAvatar, updateUserAvatar);
-router.delete('/remove-avatar', authenticate, removeUserAvatar);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+router.get("/business-users", getAllBusinessUsers);
+router.get("/me", authenticate, getMe);
+router.post("/upload-avatar", authenticate, uploadAvatar, updateUserAvatar);
+router.delete("/remove-avatar", authenticate, removeUserAvatar);
+
+// OTP Routes
+router.post("/send-otp", sendOTP);
+router.post("/verify-otp", verifyOTP);
 
 export default router;
