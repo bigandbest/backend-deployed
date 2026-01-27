0d955d9fb25c5b2570f406e4ca1037269ac3da90
Amit <vamit20000@gmail.com>
2026-01-27 21:56:17 +0530

    feat: Add `faq` field to the Product model and new wrapper methods for certification operations in the DAO.

--- dao/certification.dao.js
+++ dao/certification.dao.js
@@ -1,27 +1,44 @@
-import prisma from '../config/prisma.js';
+import prisma from "../config/prisma.js";
 
 class CertificationDAO {
-    async create(data) {
-        return await prisma.certifications.create({ data });
-    }
+  async create(data) {
+    return await prisma.certifications.create({ data });
+  }
 
-    async list(activeOnly = true) {
-        return await prisma.certifications.findMany({
-            where: activeOnly ? { active: true } : {},
-            orderBy: { sort_order: 'asc' }
-        });
-    }
+  async list(activeOnly = true) {
+    return await prisma.certifications.findMany({
+      where: activeOnly ? { active: true } : {},
+      orderBy: { sort_order: "asc" },
+    });
+  }
 
-    async update(id, data) {
-        return await prisma.certifications.update({
-            where: { id },
-            data
-        });
-    }
+  async update(id, data) {
+    return await prisma.certifications.update({
+      where: { id },
+      data,
+    });
+  }
 
-    async delete(id) {
-        return await prisma.certifications.delete({ where: { id } });
-    }
+  async delete(id) {
+    return await prisma.certifications.delete({ where: { id } });
+  }
+
+  // Wrapper methods to match controller expectations
+  async listCertifications(activeOnly = true) {
+    return this.list(activeOnly);
+  }
+
+  async createCertification(data) {
+    return this.create(data);
+  }
+
+  async updateCertification(id, data) {
+    return this.update(id, data);
+  }
+
+  async deleteCertification(id) {
+    return this.delete(id);
+  }
 }
 
 export default new CertificationDAO();
